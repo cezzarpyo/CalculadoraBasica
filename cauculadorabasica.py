69a55247fa8e27a8a8de8dec46247026dc0ea804
Júlio <jubiaskapa@gmail.com>
2024-05-18 09:41:14 -0300

    cauculadorabasica.py

--- cauculadorabasica.py
+++ cauculadorabasica.py
@@ -1,8 +1,34 @@
-import random
+def soma(a, b):
+    resultado = a + b
+    print(f"O resultado da soma é: {resultado}")
 
-bateria = random.randint(1, 100)
+def sub(a, b):
+    resultado = a - b
+    print(f"O resultado da subtração é: {resultado}")
 
-def ver_bateria():
-    print(bateria)
+def div(a, b):
+    if b == 0:
+        print("Erro: Divisão por zero não é permitida.")
+    else:
+        resultado = a / b
+        print(f"O resultado da divisão é: {resultado}")
 
-ver_bateria()
+def mul(a, b):
+    resultado = a * b
+    print(f"O resultado da multiplicação é: {resultado}")
+
+a = int(input("FALE O PRIMEIRO NUMERO: "))
+b = int(input("FALE O SEGUNDO NUMERO: "))
+c = input("FALE UM TIPO DE OPERAÇÃO (+, -, /, *): ")
+
+if c == "+":
+    soma(a, b)
+elif c == "-":
+    sub(a, b)
+elif c == "/":
+    div(a, b)
+elif c == "*":
+    mul(a, b)
+else:
+    print("Operação Inválida")
+    
